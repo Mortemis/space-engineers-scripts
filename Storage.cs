1be0968dd6755746065918bf1f3333e30576731a
Mortemis <artyom.paskin@gmail.com>
2020-10-09 13:12:24 +0400

    Add ru locale and storage sorting

--- Storage.cs
+++ Storage.cs
@@ -11,6 +11,8 @@ using Sandbox.Game;
 using VRage.Collections;
 using VRage.Game.ModAPI.Ingame;
 using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using System.Linq;
 
 namespace storage
 {
@@ -28,11 +30,15 @@ namespace storage
         const string INGOT_STORAGE_GROUP = "ingot-containers";
 
         List<IMyCargoContainer> ingots = new List<IMyCargoContainer>();
+        Dictionary<string, string> LOCALES_RU = new Dictionary<string, string>();
         IMyTextPanel panel;
+
 
 
         public Program()
         {
+            InitLocales();
+
             IMyBlockGroup ingotStorageGroup = GridTerminalSystem.GetBlockGroupWithName(INGOT_STORAGE_GROUP);
             ingotStorageGroup.GetBlocksOfType(ingots);
 
@@ -60,7 +66,7 @@ namespace storage
                 items.Add(name, mass);
             }
         }
-        
+
         StorageData GetIngotsData()
         {
             float currentVolume = 0;
@@ -84,35 +90,74 @@ namespace storage
                 inventory.GetItems(items);
                 foreach (var item in items)
                 {
-                    //data.AddItem(item.Type.ToString(), item.Amount.ToString());
-                    if (data.items.ContainsKey(item.Type.ToString()))
+                    string itemName = item.Type.ToString();
+                    itemName = itemName.Remove(0, itemName.IndexOf('/') + 1);
+                    
+                    if (data.items.ContainsKey(itemName))
                     {
-                        data.items[item.Type.ToString()] += (float)item.Amount;
-                    } 
+
+                        data.items[itemName] += (float)item.Amount;
+                    }
                     else
                     {
-                        
-                        data.AddItem(item.Type.ToString(), (float)item.Amount);
+                        data.AddItem(itemName, (float)item.Amount);
                     }
                 }
             }
-
+            
             return data;
         }
-        
+
         void UpdateDisplay()
         {
             StorageData ingotData = GetIngotsData();
-            panel.WriteText("Ingots\n");
-            foreach (var ingot in ingotData.items)
+            var ingots = from entry in ingotData.items orderby entry.Value descending select entry;
+
+            panel.WriteText("Слитки:\n");
+            foreach (var ingot in ingots)
             {
-                panel.WriteText($"{ingot.Key}: {ingot.Value}\n", true);
+                panel.WriteText($"{GetTranslation(ingot.Key)}: {NormalizeFloat(ingot.Value)}\n", true);
             }
 
         }
 
+        string NormalizeFloat(float number)
+        {
+            if (number > 1000000)
+            {
+                number /= 1000000;
+                return number.ToString("0.#") + "kk";
+            }
+            if (number > 1000)
+            {
+                number /= 1000;
+                return number.ToString("0.#") + "k";
+            }
+            return number.ToString("0");
+        }
 
+        void InitLocales()
+        {
+            LOCALES_RU.Add("Stone", "Гравий");
+            LOCALES_RU.Add("Silicon", "Кремний");
+            LOCALES_RU.Add("Iron", "Железо");
+            LOCALES_RU.Add("Nickel", "Никель");
+            LOCALES_RU.Add("Magnesium", "Магний");
+            LOCALES_RU.Add("Cobalt", "Кобальт");
+            LOCALES_RU.Add("Silver", "Серебро");
+            LOCALES_RU.Add("Gold", "Золото");
+            LOCALES_RU.Add("Platinum", "Платина");
+            LOCALES_RU.Add("Uranium", "Уран");
+        }
 
+        string GetTranslation(string word)
+        {
+            if (LOCALES_RU.ContainsKey(word))
+            {
+                return LOCALES_RU[word];
+            }
+            return word;
+        }
 
         //======================================================
         //////////////////////////END///////////////////////////
